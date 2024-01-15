dd4fd2024e9692394962828fbb0d9cd0a3235c88
Archie Jefferson Em <archie.em99@gmail.com>
2024-01-15 10:24:37 +0800

    feat: add validation

--- app/Http/Controllers/SystemAdministrator/CrewsController.php
+++ app/Http/Controllers/SystemAdministrator/CrewsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\SystemAdministrator;
 use App\Http\Controllers\Controller;
 use App\Models\Crew;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class CrewsController extends Controller
 {
@@ -18,6 +19,20 @@ class CrewsController extends Controller
     public function store(Request $request)
     {
         try {
+
+            $request->validate([
+                'rank_id' => 'required',
+                'first_name' => 'required',
+                'last_name' => 'required',
+                'email' => 'required|email',
+                'address' => 'required',
+                'birth_date' => 'required',
+                'age' => 'required',
+                'height' => 'required',
+                'weight' => 'required',
+                'age' => 'required',
+            ]);
+
             Crew::create([
                 'rank_id' => $request->rank_id,
                 'first_name' => $request->first_name,
@@ -29,10 +44,14 @@ class CrewsController extends Controller
                 'age' => $request->age,
                 'height' => $request->height,
                 'weight' => $request->weight,
-                'age' => '18',
+                'age' => $request->age,
             ]);
 
             return response()->json(['status' => 'success', 'message' => 'Successfully Crew Created.'], 201);
+        } catch (ValidationException $e) {
+            $errors = $e->errors();
+
+            return response()->json(['status' => 'error', 'errors' => $errors], 422);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
@@ -51,6 +70,32 @@ class CrewsController extends Controller
     public function update(Request $request, Crew $crew)
     {
         try {
+            $request->validate([
+                'rank_id' => 'required',
+                'first_name' => 'required',
+                'last_name' => 'required',
+                'email' => 'required|email',
+                'address' => 'required',
+                'birth_date' => 'required',
+                'age' => 'required',
+                'height' => 'required',
+                'weight' => 'required',
+                'age' => 'required',
+            ]);
+
+            Crew::create([
+                'rank_id' => $request->rank_id,
+                'first_name' => $request->first_name,
+                'middle_name' => $request->middle_name,
+                'last_name' => $request->last_name,
+                'email' => $request->email,
+                'address' => $request->address,
+                'birth_date' => $request->birth_date,
+                'age' => $request->age,
+                'height' => $request->height,
+                'weight' => $request->weight,
+                'age' => $request->age,
+            ]);
             $crew->update([
                 'rank_id' => $request->rank_id,
                 'first_name' => $request->first_name,
@@ -66,6 +111,10 @@ class CrewsController extends Controller
             ]);
 
             return response()->json(['status' => 'success', 'message' => 'Successfully Crew Updated.'], 200);
+        } catch (ValidationException $e) {
+            $errors = $e->errors();
+
+            return response()->json(['status' => 'error', 'errors' => $errors], 422);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
