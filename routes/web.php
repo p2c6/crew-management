35961d5d34be1f005370f04ba69dd34e15a0c22e
Archie Jefferson Em <archie.em99@gmail.com>
2024-01-15 09:49:30 +0800

    feature: include allowing of accessing url

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\SystemAdministrator\CrewsController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('{reactRoutes}', function () {
-//     return view('app');
-// })->where('reactRoutes', '^((?|api).)*$');
-
 Route::get('/{path?}', function () {
     return view('app');
-})->where('path', '.*');
+})->where('path', '^(?!storage|public).*$');
